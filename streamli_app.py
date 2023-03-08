9f24d0b169e044f6c39059e3579d8a1916811092
khalid1011198 <127181377+khalid1011198@users.noreply.github.com>
2023-03-08 14:45:36 +0530

    Update streamli_app.py
    added pandas functionality

--- streamli_app.py
+++ streamli_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy diner')
 streamlit.header('Lets see breakfast Menu')
 streamlit.text('🐟Fish oil')
@@ -6,3 +7,6 @@ streamlit.text('🍀Spinach')
 streamlit.text('🥚Eggs')
 
 streamlit.header('Build your own smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
