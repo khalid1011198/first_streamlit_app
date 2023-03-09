3b2578b8f65033a252933d109fb16c8c1e4bd790
khalid1011198 <127181377+khalid1011198@users.noreply.github.com>
2023-03-09 13:08:23 +0530

    Update streamli_app.py
    removed json preview

--- streamli_app.py
+++ streamli_app.py
@@ -20,7 +20,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 # normalizing json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display as table
